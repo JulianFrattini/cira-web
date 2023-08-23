b0f75a2632bcca63698a6774cb3c9bec0dde9979
Julian Frattini <julian.frattini@alice-dsl.net>
2023-08-23 15:47:06 +0200

    Setup basic CiRA UI

---
+++ frontend/src/components/cira-core/Core.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from 'react'
+
+import '../../stylesheets/form.css'
+
+import LabelVisualizer from '../../packages/labelvisualization/LabelVisualizer'
+import CEGVisualizer from '../../packages/graphvisualization/CEGVisualizer'
+import SuiteVisualizer from '../../packages/suitevisualization/SuiteVisualizer'
+
+
+function Core() {
+    const [serverResponding, setServerResponding] = useState(false)
+
+    const [fieldSentence, setFieldSentence] = useState("When the red button is pushed or the power fails the system shuts down.")
+
+    const [analyzing, setAnalyzing] = useState(false)
+
+    const [sentence, setSentence] = useState("")
+    const [labels, setLabels] = useState([])
+    const [ceg, setCeg] = useState(null)
+    const [suite, setSuite] = useState(null)
+
+    useEffect(() => {
+        if (sentence) {
+            fetch('http://localhost:8000/api/label', {
+                method: 'PUT',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ "sentence": sentence })
+            }).then(res => res.json())
+                .then(jsonlabels => setLabels(jsonlabels.labels))
+                .then(() => {
+                    return fetch('http://localhost:8000/api/graph', {
+                        method: 'PUT',
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify({
+                            "sentence": sentence,
+                            "labels": labels.labels
+                        })
+                    });
+                })
+                .then(res => res.json())
+                .then(graph => {
+                    setCeg(graph.graph);
+
+                    return fetch('http://localhost:8000/api/testsuite', {
+                        method: 'PUT',
+                        headers: {"Content-Type": "application/json"},
+                        body: JSON.stringify({
+                            "sentence": sentence,
+                            "graph": graph.graph
+                        })
+                    });
+                })
+                .then(res => res.json())
+                .then(testsuite => setSuite(testsuite.suite))
+                .then(() => { setAnalyzing(false) })
+                .catch(function (error) {
+                    console.error(error)
+                });
+        }
+    }, [sentence]);
+
+    // heartbeat
+    useEffect(() => {
+        fetch('http://localhost:8000/api/health', {
+            method: 'get'
+        }).then(res => res.json())
+            .then(heartbeat => {
+                if (Object.hasOwn(heartbeat, "status")) {
+                    setServerResponding(true);
+                }
+            })
+            .catch(function (error) {
+                console.error(error)
+            });
+    }, [])
+
+    const analyze = (e) => {
+        e.preventDefault();
+
+        setAnalyzing(true)
+        setLabels([])
+        setCeg({})
+        setSuite(null)
+        setSentence(fieldSentence)
+    }
+
+    return (
+        <div className="core">
+            <h2>Step 1: Classification</h2>
+            <p>Enter a causal requirements and click 'analyze' to let CiRA automatically determine, whether the requirement is causal and hence eligible for further processing. You can find more details on the classifier in <a href='https://arxiv.org/abs/2101.10766' target="_blank" rel="noreferrer">our corresponding paper</a>.</p>
+            <form className='submit-form' onSubmit={(e) => analyze(e)}>
+                <input type='text' id='sentence' onChange={e => setFieldSentence(e.target.value)} value={fieldSentence} autoFocus></input>
+                <input type="submit" value='Classify' disabled={!serverResponding}></input>
+            </form>
+            {!analyzing && labels.length > 0 &&
+                <div>
+                    <h2>Step 2: Labeling</h2>
+                    <LabelVisualizer text={sentence} labels={labels}></LabelVisualizer>
+                </div>
+            }
+            {!analyzing && ceg != null &&
+                <div>
+                    <h2>Cause-Effect-Graph</h2>
+                    <CEGVisualizer ceg={ceg}></CEGVisualizer>
+                </div>
+            }
+            {!analyzing && suite != null && 
+                <div>
+                    <h2>Test Suite</h2>
+                    <SuiteVisualizer suite={suite}></SuiteVisualizer>
+                </div>
+            }
+        </div>
+    );
+}
+
+export default Core;
--- frontend/src/pages/demo.js
+++ frontend/src/pages/demo.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
+import Core from '../components/cira-core/Core';
  
 const Demo = () => {
     return (
         <div>
             <h2>Causality Extraction: Demo</h2>
-            <p>This page demonstrates the automatic, step-wise extraction of cause-effect relations from natural language sentences.</p>
+            <p>On this page you can explore the automatic, step-wise extraction of cause-effect relations from natural language sentences through our accessible online demo. The code behind this demonstration is open source and publicly available on <a href='https://github.com/JulianFrattini/cira' target="_blank" rel="noreferrer">GitHub</a>.</p>
+            <Core></Core>
         </div>
     );
 };
