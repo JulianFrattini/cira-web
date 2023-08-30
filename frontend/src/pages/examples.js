701d8d28f931c9a791e3256f7815c924068800e5
Julian Frattini <julian.frattini@alice-dsl.net>
2023-08-30 17:02:14 +0200

    Improved examples page

--- frontend/src/pages/examples.js
+++ frontend/src/pages/examples.js
@@ -1,8 +1,9 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import sentences from './../data/sentences.json'
 
 import LabelVisualizer from "../packages/labelvisualization/LabelVisualizer";
+import CEGVisualizer from "../packages/graphvisualization/CEGVisualizer";
 
 const Examples = () => {
     const [switching, setSwitching] = useState(false);
@@ -10,11 +11,22 @@ const Examples = () => {
 
     const [sentence, setSentence] = useState(sentences[0].text);
     const [labels, setLabels] = useState(sentences[0].labels);
+    const [ceg, setCeg] = useState(sentences[0].graph);
+    const [cegdesc, setCegdesc] = useState("");
 
     useEffect(() => {
-        let index = parseInt(selected)-1;
+        let index = sentences.indexOf(sentences.find((sentence) => sentence.id === selected));
+        
         setSentence(sentences[index].text);
         setLabels(sentences[index].labels);
+        
+        setCeg(sentences[index].graph);
+        if('desc' in sentences[index].graph) {
+            setCegdesc(sentences[index].graph.desc);
+        } else {
+            setCegdesc("");
+        }
+
         setSwitching(false);
     }, [selected])
 
@@ -24,22 +36,35 @@ const Examples = () => {
             <p>This page illustrates and describes some examples for the <a href='/demo'>causality extraction demo</a> for automatic test case generation.</p>
 
             <div>
-                <select value={selected} onChange={e => {setSelected(e.target.value); setSwitching(true);}}>
+                <select value={selected} onChange={e => {
+                    setSelected(e.target.value);
+                    setSwitching(true);
+                }}>
                     {
                         sentences.map(sentence =>
-                            <option key={sentence.id} value={sentence.id}>{sentence.title} {}</option>
+                            <option key={sentence.id} value={sentence.id}>{sentence.title} { }</option>
                         )
                     }
                 </select>
             </div>
 
             <p><b>Example sentence: </b>{sentence}</p>
-            
+
             <h3>Labels</h3>
-            {(!switching && sentence != null && labels.length > 0) ? 
+            <p>CiRA labels the sentence as follows:</p>
+            {(!switching && sentence != null && labels.length > 0) ?
                 <LabelVisualizer text={sentence} labels={labels}></LabelVisualizer> :
                 <p>Labels loading ... </p>
             }
+
+            <h3>Cause-Effect-Graph</h3>
+            {cegdesc ? 
+                <p>{cegdesc}</p> : 
+                <p>no desc</p>}
+            {(ceg != null) ?
+                <CEGVisualizer ceg={ceg}></CEGVisualizer> :
+                <p>Cause-Effect Graph loading ... </p>
+        }
         </div>
     );
 };
