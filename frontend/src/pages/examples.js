fe0aceeb3f6dfac53e504cd67a1f1266b388ee90
Julian Frattini <julian.frattini@alice-dsl.net>
2023-08-30 22:17:42 +0200

    Completed sentence conversion and visualization (closes #2)

--- frontend/src/pages/examples.js
+++ frontend/src/pages/examples.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 
-import sentences from './../data/sentences.json'
+import sentences from './../data/sentences2.json'
 
 import LabelVisualizer from "../packages/labelvisualization/LabelVisualizer";
 import CEGVisualizer from "../packages/graphvisualization/CEGVisualizer";
+import SuiteVisualizer from "../packages/suitevisualization/SuiteVisualizer";
 
 const Examples = () => {
     const [switching, setSwitching] = useState(false);
@@ -11,8 +12,12 @@ const Examples = () => {
 
     const [sentence, setSentence] = useState(sentences[0].text);
     const [labels, setLabels] = useState(sentences[0].labels);
+
     const [ceg, setCeg] = useState(sentences[0].graph);
     const [cegdesc, setCegdesc] = useState("");
+
+    const [suite, setSuite] = useState(sentence[0].testsuite);
+    const [suitedesc, setSuitedesc] = useState("");
 
     useEffect(() => {
         let index = sentences.indexOf(sentences.find((sentence) => sentence.id === selected));
@@ -24,7 +29,14 @@ const Examples = () => {
         if('desc' in sentences[index].graph) {
             setCegdesc(sentences[index].graph.desc);
         } else {
-            setCegdesc("");
+            setCegdesc("CiRA constructs the following cause-effect graph from the labeled sentence:");
+        }
+
+        setSuite(sentences[index].testsuite);
+        if('desc' in sentences[index].testsuite && sentences[index].testsuite.desc) {
+            setSuitedesc(sentences[index].testsuite.desc);
+        } else {
+            setSuitedesc("Based on the cause-effect graph, the following, minimal set of test cases ensures full coverage of the initial requirement:");
         }
 
         setSwitching(false);
@@ -58,13 +70,17 @@ const Examples = () => {
             }
 
             <h3>Cause-Effect-Graph</h3>
-            {cegdesc ? 
-                <p>{cegdesc}</p> : 
-                <p>no desc</p>}
+            <p>{cegdesc}</p>
             {(ceg != null) ?
                 <CEGVisualizer ceg={ceg}></CEGVisualizer> :
                 <p>Cause-Effect Graph loading ... </p>
-        }
+            }
+
+            <h3>Acceptance Tests</h3>
+            <p>{suitedesc}</p>
+            {(suite != null) ? 
+                <SuiteVisualizer suite={suite}></SuiteVisualizer> :
+                <p>Test suite loading ...</p>}
         </div>
     );
 };
