461982b8e63aca1051eb708adfe5758eaa8850bd
Julian Frattini <julian.frattini@alice-dsl.net>
2023-08-31 08:26:59 +0200

    Moved navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 import './App.css';
 
 import Headline from './components/headline/index';
-import Navbar from './components/navbar/index';
+//import Navbar from './components/navbar/index';
+import './stylesheets/navbar.css'
 
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/index';
@@ -16,15 +17,30 @@ function App() {
     return (
         <div>
             <Headline />
-            <Navbar />
+            <div className="nav">
+                <div className="navmenu">
+                    <a className="navlink" href="/" activestyle="true">
+                        Home
+                    </a>
+                    <a className="navlink" href="/demo" activestyle="true">
+                        Demonstration
+                    </a>
+                    <a className="navlink" href="/examples" activestyle="true">
+                        Examples
+                    </a>
+                    <a className="navlink" href="/references" activestyle="true">
+                        References
+                    </a>
+                </div>
+            </div>
             <Router>
                 <Routes>
                     <Route exact path='/' element={<Home />} />
                     <Route path='/demo' element={<Demo />} />
                     <Route path='/examples' element={<Examples />} />
                     <Route path='/references' element={<References />} />
-                    
-                    <Route path='/bert' element={<Navigate replace to='/demo'/>} />
+
+                    <Route path='/bert' element={<Navigate replace to='/demo' />} />
                 </Routes>
             </Router>
         </div>
